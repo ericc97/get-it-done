d71eb3c7af54955adb68329476b2cf91b9fd09b5
Eric Coleman <ericcoleman9217@gmail.com>
2021-11-11 15:43:03 -0600

    added new html/js to view repos/ repo issues. Added link to view additonal repos if more than 30 is present

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo){
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,12 +10,31 @@ var getRepoIssues = function(repo){
             response.json().then(function(data){
                 // pass response data to dom function
                 listIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("link")) {
+                    displayWarning(repo);
+                }
             });
         }else {
             alert("There was a problem with your request!");
         }
     });
 }
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    // set to open in a new tab
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 
 var listIssues = function(issues) {
@@ -54,8 +74,7 @@ var listIssues = function(issues) {
         issueEl.setAttribute("target", "_blank");
 
         issueContainerEl.appendChild(issueEl);
-
     }
 };
 
-getRepoIssues("facebook/react");
+getRepoIssues("angular/angular");
