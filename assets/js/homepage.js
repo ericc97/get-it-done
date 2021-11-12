56e6860e6c348239ab4a671526ad53f81c8840a6
Eric Coleman <ericcoleman9217@gmail.com>
2021-11-12 11:13:42 -0600

    added JS,HTML,CSS search function to find related issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repo-containers');
 var repoSearchTerm = document.querySelector('#repo-search-term');
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -43,6 +43,16 @@ var formSubmitHandler = function(event) {
     //console.log(event);
 };
 
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+}
 
 
 var displayRepos = function(repos, searchTerm) {
@@ -93,5 +103,20 @@ var displayRepos = function(repos, searchTerm) {
 
 };
 
+var getFeaturedRepos = function(language){
+    var apiUrl= "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    
+    fetch(apiUrl).then(function(response){
+        if(response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: Github User Not Found');
+        }
+    });
+};
+
 // add event listeners to form
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
