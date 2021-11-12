b0cbf4502e771d5de3b8cb0a7792a018c2be5a54
Eric Coleman <ericcoleman9217@gmail.com>
2021-11-11 18:49:13 -0600

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,14 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
 
 var getRepoIssues = function(repo){
+    // format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response){
         // request was successful
         if(response.ok){
@@ -14,13 +19,30 @@ var getRepoIssues = function(repo){
                 // check if api has paginated issues
                 if (response.headers.get("link")) {
                     displayWarning(repo);
+                }else {
+                    document.location.replace("./index.html");
                 }
             });
-        }else {
-            alert("There was a problem with your request!");
         }
     });
 }
+
+var getRepoName = function() {
+
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    }else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -43,10 +65,15 @@ var listIssues = function(issues) {
         issueContainerEl.textContent = "This Repo has no open issues!";
         return;
     }
-
+    // loop over given issues
     for(var i=0; i < issues.length; i++) {
         // create a link element to take users to the issues on github
-        var issueEl = document.querySelector("a");
+        var issueEl = document.createElement("a");
+
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        // set to open link in a new tab
+        issueEl.setAttribute("target", "_blank");
         
         //create span to hold issue title
         var titleEl = document.createElement("span");
@@ -68,13 +95,8 @@ var listIssues = function(issues) {
         //append to container
         issueEl.appendChild(typeEl);
 
-        issueEl.classList = "list-item flex-row justify-space-between align-center";
-        issueEl.setAttribute("href", issues[i].html_url);
-        // set to open link in a new tab
-        issueEl.setAttribute("target", "_blank");
-
         issueContainerEl.appendChild(issueEl);
     }
 };
 
-getRepoIssues("angular/angular");
+getRepoName();
